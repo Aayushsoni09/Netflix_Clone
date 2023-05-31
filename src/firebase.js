09ea372de3d9122bf322ade85516f00b1340857c
Aayushsoni09 <44709621+Aayushsoni09@users.noreply.github.com>
2023-06-01 01:00:51 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,6 @@
 
 import firebase from "firebase";
+import { initializeAppCheck, ReCaptchaV3Provider } from "firebase/app-check";
 const firebaseApp = firebase.initializeApp({
     apiKey: "AIzaSyAUyqENcFSmU59rwuZGQ6B2n6eZUtxOW74",
     authDomain: "netflix-clone-e4069.firebaseapp.com",
@@ -9,7 +10,13 @@ const firebaseApp = firebase.initializeApp({
     appId: "1:286720558500:web:7fe8d22c67e6b7b8ef6d31",
     measurementId: "G-6TXKP4S8NK"
 });
+const appCheck = initializeAppCheck(app, {
+  provider: new ReCaptchaV3Provider('6LclslcmAAAAACi1QW-nU506aK_glIeFwrS9zS7v'),
 
+  // Optional argument. If true, the SDK automatically refreshes App Check
+  // tokens as needed.
+  isTokenAutoRefreshEnabled: true
+});
 const db =firebaseApp.firestore();
 const auth=firebase.auth();
 export{auth}
